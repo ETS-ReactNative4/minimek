0a9c7806d3e80e8bef6a516b1e2951f31f5096ed
vamsiampolu <vamsiampolu3000@gmail.com>
2018-09-06 16:25:28 +1000

    Connected unit-info to the redux store and ensured that we can load data from mock API

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux'
 
 import tabs from "features/tabs/reducer";
+import unitInfo from 'features/unit-info/unitInfoReducer'
 
 const rootReducer = combineReducers({
-    tabs
+    tabs,
+    unitInfo
 })
 
 export default rootReducer
--- src/features/unit-info/UnitInfo.js
+++ src/features/unit-info/UnitInfo.js
@@ -1,18 +1,29 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
 
 import { Segment, Dropdown, Form } from 'semantic-ui-react'
 
-export default function UnitInfo(props) {
+
+import { selectUnitInfo } from 'features/unit-info/unitInfoSelector'
+
+export function UnitInfo(props) {
   return (<Segment attached="bottom">
     <Form size="large">
       <Form.Field name="name" width={6}>
         <label>Unit Name</label>
-        <input placeholder="Name" />
+        <input 
+          placeholder="Name"
+          value={props.unitInfo.name}
+        />
       </Form.Field>
       <Form.Field name="affiliations" width={6}>
         <label>Affiliations</label>
-        <Dropdown selection options={props.factions} />
+        <Dropdown 
+          selection 
+          options={props.factions}
+          value={props.unitInfo.affiliation}
+        />
       </Form.Field>
     </Form>
   </Segment>)
@@ -32,5 +43,14 @@ UnitInfo.defaultProps = {
         {value : "fs", text : "Federated Suns"},
         {value : "fwl", text : "Free Worlds League"},
         {value : "lc", text : "Lyran Commonwealth"},
+        {value : "wd", text : "Wolf's Dragoons"}
     ]
 }
+
+function mapStateToProps(state) {
+  return {
+    unitInfo: selectUnitInfo(state)
+  }
+}
+
+export default connect(mapStateToProps)(UnitInfo)
